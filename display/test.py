299973e9714d2798465fd8af4c9f9273d7eda38b
aidanchandra <aidanchandra@gmail.com>
2022-01-17 11:32:25 -0800

    More work

--- display/test.py
+++ display/test.py
@@ -1,10 +1,11 @@
+from doctest import UnexpectedException
 from enum import Enum
 from statistics import mode
 import time
 from turtle import width
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image,ImageTk,ImageDraw, ImageFont
+import tkinter
+
 
 class image_interface:
 
@@ -24,6 +25,7 @@ class image_interface:
         self.height = 64
 
         #Libraries available on every OS - regardless of testing
+
         try:
             import RPi.GPIO as GPIO
             import Adafruit_GPIO.SPI as SPI
@@ -40,61 +42,97 @@ class image_interface:
             self.mode = self.modes.LIVE
         except ImportError:
             self.mode = self.modes.TEST
-            self.disp - None
+            self.disp = None
         except Exception:
-
+            print("There must be an error in establishing connection")
             exit(0)
 
 
-        self.image = Image.new('1', (self,width, self.height))
+        self.image = Image.new('1', (self.width, self.height))
 
         if self.mode == self.modes.TEST:
-            print("a")
+            #Start a threat with the display updating
+
+            self.root = tkinter.Tk()
+            self.label = tkinter.Label(self.root)
+            self.label.pack()
+            self.tkimg = [None]  
+            delay = 50 
+            def loopCapture():
+
+                #'''
+                draw = ImageDraw.Draw(self.image)
+                draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+                draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  #Up filled
+                #'''
+
+                self.tkimg[0] = ImageTk.PhotoImage(self.image)
+                self.label.config(image=self.tkimg[0])
+                self.root.update_idletasks()
+                self.root.after(delay, loopCapture)
+            loopCapture()
+            self.root.mainloop()
+            
 
 
     def update_image(self, image:Image):
-        self.image = image
+        self.tkimg[1] = ImageTk.PhotoImage(image)
+
+        self.label.config(image=self.tkimg[1])
+
+        if self.mode == self.modes.LIVE:
+            self.update_screen()
+
+    def update_screen(self):
+        if self.mode != self.modes.LIVE:
+            raise UnexpectedException("This method should only ever be called in modes.LIVE")
+        else:
+            print("Have yet to test :)")
+            self.disp.image(self.image)
+            self.disp.display() 
         
 
     def get_image(self):
         return self.image
 
 
-import tkinter
-global i
-i = 0
-root = tkinter.Tk()
-label = tkinter.Label(root)
-label.pack()
-img = None
-tkimg = [None]  # This, or something like it, is necessary because if you do not keep a reference to PhotoImage instances, they get garbage collected.
+# global i
+# i = 0
+# root = tkinter.Tk()
+# label = tkinter.Label(root)
+# label.pack()
+# img = None
+# tkimg = [None]  # This, or something like it, is necessary because if you do not keep a reference to PhotoImage instances, they get garbage collected.
+# delay = 50   # in milliseconds
+# def loopCapture():
+#     global i
+#     print("capturing")
+#     width = 128#disp.width
+#     height = 64#disp.height
+#     image = Image.new('1', (width, height))
+
+#     # Get drawing object to draw on image.
+#     draw = ImageDraw.Draw(image)
+
+#     # Draw a black filled box to clear the image.
+#     draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+#     i += 1
+#     draw.polygon([(20, 20), (30, 2), (40, 20+i)], outline=255, fill=1)  #Up filled
 
 
+#     tkimg[0] = ImageTk.PhotoImage(image)
+#     label.config(image=tkimg[0])
+#     root.update_idletasks()
+#     root.after(delay, loopCapture)
+# loopCapture()
+# root.mainloop()
 
+if __name__ == "__main__":
+    display_driver = image_interface()
 
-
-delay = 50   # in milliseconds
-def loopCapture():
-    global i
-    print("capturing")
-    width = 128#disp.width
-    height = 64#disp.height
-    image = Image.new('1', (width, height))
-
-    # Get drawing object to draw on image.
+    image = display_driver.get_image()
     draw = ImageDraw.Draw(image)
+    draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  #Up filled
 
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-    i += 1
-    draw.polygon([(20, 20), (30, 2), (40, 20+i)], outline=255, fill=1)  #Up filled
-
-
-    tkimg[0] = ImageTk.PhotoImage(image)
-    label.config(image=tkimg[0])
-    root.update_idletasks()
-    root.after(delay, loopCapture)
-
-loopCapture()
-root.mainloop()
+    display_driver.update_image(image)
